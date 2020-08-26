183898e83894254a816b54b8fc7a07af79e96a2f
BoziDaniel <daniel.bozi88@gmail.com>
2020-08-26 15:47:09 +0200

    implemented hide activeorders button while user is not logged in

--- src/components/commonComponents/Navbar.js
+++ src/components/commonComponents/Navbar.js
@@ -1,21 +1,30 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 import { Button } from "antd";
-
+import { LoginContext } from "../context/LoginContext";
 const Navbar = () => {
+  const { LoggedInAsRole } = useContext(LoginContext);
+
   return (
     <div>
       <Link className="link" to="/">
-        <Button size={"large"}><b>Pizzas</b></Button>
+        <Button size={"large"}>
+          <b>Pizzas</b>
+        </Button>
       </Link>
 
       <Link className="link" to="/contact">
-        <Button  size={"large"}><b>Contact</b></Button>
+        <Button size={"large"}>
+          <b>Contact</b>
+        </Button>
       </Link>
-
-      <Link className="link" to="/orders/active">
-        <Button  size={"large"}><b>Active Orders</b></Button>
-      </Link>
+      {LoggedInAsRole === "" ? null : (
+        <Link className="link" to="/orders/active">
+          <Button size={"large"}>
+            <b>Active Orders</b>
+          </Button>
+        </Link>
+      )}
     </div>
   );
 };
