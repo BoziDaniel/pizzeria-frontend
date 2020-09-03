6910fc5586215393b81c4ba08a8d8fa1accadf7e
BoziDaniel <daniel.bozi88@gmail.com>
2020-09-03 10:02:01 +0200

    changed phone icon color in registration from red to black

--- src/components/commonComponents/registration/RegistrationForm.js
+++ src/components/commonComponents/registration/RegistrationForm.js
@@ -1,37 +1,77 @@
-import React from 'react';
+import React from "react";
 import { Input, Row } from "antd";
-import { UserOutlined } from '@ant-design/icons';
+import { UserOutlined, PhoneOutlined } from "@ant-design/icons";
 
 const RegistrationForm = (props) => {
-    const handleNameChange = (e) => {
-        props.setName(e.target.value);
-    };
-    const handleUsernameChange = (e) => {
-        props.setUsername(e.target.value);
-    };
-    const handleEmailChange = (e) => {
-        props.setEmail(e.target.value);
-    };
-    const handlePhoneNumberChange = (e) => {
-        props.setPhoneNumber(e.target.value);
-    };
-    const handlePasswordChange = (e) => {
-        props.setPassword(e.target.value);
-    };
-    const handleConfirmPasswordChange = (e) => {
-        props.setConfirmPassword(e.target.value);
-    };
-   
-    return (
-        <div>
-            <Row>Name: <Input placeholder="example: Kovács Béla" prefix={<UserOutlined />} onChange={handleNameChange}/></Row>
-            <Row>Username: <Input placeholder="example: pizzaman300" prefix={<UserOutlined />}  onChange={handleUsernameChange}/></Row>
-            <Row>Email: <Input placeholder="example: pizzaman300@gmail.com" prefix={<div>@</div>}  onChange={handleEmailChange}/></Row>
-            <Row>Phone: <Input placeholder="example: +36-70-9443-402" prefix={<div>&#9742;</div>}  onChange={handlePhoneNumberChange}/></Row>
-            <Row>Password: <Input.Password placeholder="input password"  onChange={handlePasswordChange}/></Row>
-            <Row>Confirm password: <Input.Password placeholder="input password"  onChange={handleConfirmPasswordChange}/></Row>
-        </div>
-    )
-}
+  const handleNameChange = (e) => {
+    props.setName(e.target.value);
+  };
+  const handleUsernameChange = (e) => {
+    props.setUsername(e.target.value);
+  };
+  const handleEmailChange = (e) => {
+    props.setEmail(e.target.value);
+  };
+  const handlePhoneNumberChange = (e) => {
+    props.setPhoneNumber(e.target.value);
+  };
+  const handlePasswordChange = (e) => {
+    props.setPassword(e.target.value);
+  };
+  const handleConfirmPasswordChange = (e) => {
+    props.setConfirmPassword(e.target.value);
+  };
 
-export default RegistrationForm
+  return (
+    <div>
+      <Row>
+        Name:{" "}
+        <Input
+          placeholder="example: Kovács Béla"
+          prefix={<UserOutlined />}
+          onChange={handleNameChange}
+        />
+      </Row>
+      <Row>
+        Username:{" "}
+        <Input
+          placeholder="example: pizzaman300"
+          prefix={<UserOutlined />}
+          onChange={handleUsernameChange}
+        />
+      </Row>
+      <Row>
+        Email:{" "}
+        <Input
+          placeholder="example: pizzaman300@gmail.com"
+          prefix={<div>@</div>}
+          onChange={handleEmailChange}
+        />
+      </Row>
+      <Row>
+        Phone:{" "}
+        <Input
+          placeholder="example: +36-70-9443-402"
+          prefix={<PhoneOutlined />}
+          onChange={handlePhoneNumberChange}
+        />
+      </Row>
+      <Row>
+        Password:{" "}
+        <Input.Password
+          placeholder="input password"
+          onChange={handlePasswordChange}
+        />
+      </Row>
+      <Row>
+        Confirm password:{" "}
+        <Input.Password
+          placeholder="input password"
+          onChange={handleConfirmPasswordChange}
+        />
+      </Row>
+    </div>
+  );
+};
+
+export default RegistrationForm;
