d2e59473cd6338c3fe21cbea1c829eecaaba2f98
BoziDaniel <daniel.bozi88@gmail.com>
2020-09-10 17:14:20 +0200

    implemnted basic input validation for registration. More password checks required!

--- src/components/commonComponents/registration/RegistrationForm.js
+++ src/components/commonComponents/registration/RegistrationForm.js
@@ -13,15 +13,40 @@ const RegistrationForm = (props) => {
   };
   const handleEmailChange = (e) => {
     props.setEmail(e.target.value);
+    checkIsEmailValid(e.target.value);
   };
   const handlePhoneNumberChange = (e) => {
     props.setPhoneNumber(e.target.value);
+    checkIsPhoneNumberValid(e.target.value);
   };
   const handlePasswordChange = (e) => {
     props.setPassword(e.target.value);
+    checkArePasswordsMatch();
+    checkIsPasswordHasMinLength(e.target.value);
   };
   const handleConfirmPasswordChange = (e) => {
     props.setConfirmPassword(e.target.value);
+  };
+  const checkIsEmailValid = (email) => {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    const emailIsValid = re.test(String(email).toLowerCase());
+    props.setIsEmailValid(emailIsValid);
+  };
+
+  const checkArePasswordsMatch = () => {
+    const passwordsAreTheSame = props.password === props.confirmPassword;
+    props.setArePasswordsMatch(passwordsAreTheSame);
+  };
+
+  const checkIsPhoneNumberValid = (phoneNumber) => {
+    const re = /[0-9]{2,2}-[0-9]{2,2}-[0-9]{3,3}-[0-9]{4,4}/;
+    const phoneIsValid = re.test(String(phoneNumber).toLowerCase());
+    props.setIsPhoneNumberValid(phoneIsValid);
+  };
+
+  const checkIsPasswordHasMinLength = (password) => {
+    const passWordHasMinLength = password.length > 3;
+    props.setIsPasswordHasMinLength(passWordHasMinLength);
   };
 
   const checkIfUsernameNotOccupied = (username) => {
--- src/components/commonComponents/registration/RegistrationModal.js
+++ src/components/commonComponents/registration/RegistrationModal.js
@@ -4,7 +4,6 @@ import RegistrationForm from "./RegistrationForm";
 import axios from "axios";
 
 const RegistrationModal = () => {
-  //username password email phoneNumber name
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
@@ -12,7 +11,11 @@ const RegistrationModal = () => {
   const [phoneNumber, setPhoneNumber] = useState("");
   const [name, setName] = useState("");
   const [visible, setVisible] = useState(false);
-  const [isUsernameNotOccupied,setIsUsernameNotOccupied] = useState(false);
+  const [isUsernameNotOccupied, setIsUsernameNotOccupied] = useState(false);
+  const [isEmailValid, setIsEmailValid] = useState(false);
+  const [arePasswordsMatch, setArePasswordsMatch] = useState(false);
+  const [isPhoneNumberValid, setIsPhoneNumberValid] = useState(false);
+  const [isPasswordHasMinLength, setIsPasswordHasMinLength] = useState(false);
   const createUserData = () => {
     const userData = {
       username: username,
@@ -23,6 +26,7 @@ const RegistrationModal = () => {
     };
     return userData;
   };
+
   const initialModalText = (
     <RegistrationForm
       username={username}
@@ -38,6 +42,14 @@ const RegistrationModal = () => {
       name={name}
       setName={setName}
       setIsUsernameNotOccupied={setIsUsernameNotOccupied}
+      isEmailValid={isEmailValid}
+      setIsEmailValid={setIsEmailValid}
+      arePasswordsMatch={arePasswordsMatch}
+      setArePasswordsMatch={setArePasswordsMatch}
+      isPhoneNumberValid={isPhoneNumberValid}
+      setIsPhoneNumberValid={setIsPhoneNumberValid}
+      isPasswordHasMinLength={isPasswordHasMinLength}
+      setIsPasswordHasMinLength={setIsPasswordHasMinLength}
     />
   );
   const [ModalText, setModalText] = useState(initialModalText);
@@ -47,38 +59,26 @@ const RegistrationModal = () => {
     setVisible(true);
   };
 
-  const isEmailValid = () => {
-    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(String(email).toLowerCase());
-  }
-
-  const arePasswordsMatch = () => {
-    return password === confirmPassword;
-  }
-  
-  const isPhoneNumberValid = () => {
-    const re = /[0-9]{2}-[0-9]{2}-[0-9]{3}-[0-9]{4}/;
-    return re.test(String(phoneNumber).toLowerCase());
-  }
-
-  const isPasswordHasMinLength = () => {
-    return password.length> 3;
-  }
-
   const areInputsValid = () => {
+    console.log("is email valid: " + isEmailValid);
+    console.log("phone number is valid " + isPhoneNumberValid);
+    console.log("are passwords the same: " + arePasswordsMatch);
+    console.log(
+      "password is at least 3 characters long " + isPasswordHasMinLength
+    );
+    console.log("username is not occupied " + isUsernameNotOccupied);
     return (
-    isEmailValid() &&
-    arePasswordsMatch() &&
-    isPhoneNumberValid() &&
-    isPasswordHasMinLength() &&
-    isUsernameNotOccupied)
-  }
-
-  
+      isEmailValid &&
+      arePasswordsMatch &&
+      isPhoneNumberValid &&
+      isPasswordHasMinLength &&
+      isUsernameNotOccupied
+    );
+  };
 
   const handleOk = () => {
     const user = createUserData();
-    if(areInputsValid()){
+    if (areInputsValid()) {
       const options = {
         url: "http://localhost:8080/registration",
         method: "POST",
@@ -102,17 +102,15 @@ const RegistrationModal = () => {
           setModalText(resp.status + " error during registration");
         }
       });
-    }else{
+    } else {
       alert("invalid input try again");
     }
-    
   };
 
   const handleCancel = () => {
     console.log("Clicked cancel button");
     setVisible(false);
   };
-
   return (
     <div>
       <Button type="primary" onClick={showModal} style={{ marginRight: 5 }}>
